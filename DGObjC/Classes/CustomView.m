ae44327be01fd72ab7ff4878624994d226ed0429
lixiaodongdg <lixiaodongdg@163.com>
2018-10-30 11:29:23 +0800

    更新

--- DGObjC/Classes/CustomView.m
+++ DGObjC/Classes/CustomView.m
@@ -6,19 +6,15 @@
 //
 
 #import "CustomView.h"
-//#import <DGUIKit/CView.h>
 
 
 @implementation CustomView
 - (void)logView {
-    NSLog(@"AAAAAAAAAAAA");
-    NSLog(@"BBBBBBBBBBBB");
     UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor redColor];
     [self addSubview:view];
-    NSLog(@"ZZZZZZZ");
-    NSLog(@"5.5.0");
-    NSLog(@"6.5.0");
-    NSLog(@"test code");
+    
+    NSLog(@"AAAAA");
 }
 
 - (void)log {
