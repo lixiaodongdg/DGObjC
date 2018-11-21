8a41c06602608b46352a0b6e801c6528a7b34090
lixiaodongdg <lixiaodongdg@163.com>
2018-11-21 15:28:20 +0800

    添加

---
+++ DGObjC/Classes/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomUIView.h
+//  DGObjC
+//
+//  Created by lxd on 2018/5/7.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+- (void)logView;
+- (void)log;
+@end
---
+++ DGObjC/Classes/CustomView.m
@@ -0,0 +1,23 @@
+//
+//  CustomUIView.m
+//  DGObjC
+//
+//  Created by lxd on 2018/5/7.
+//
+
+#import "CustomView.h"
+
+
+@implementation CustomView
+- (void)logView {
+    NSLog(@"111111111111");
+}
+
+
+- (void)log {
+    NSLog(@"222222222");
+    NSLog(@"master");
+}
+
+@end
+
